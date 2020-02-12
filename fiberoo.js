dc6a9a876f7506f05fb6f7868bf2601f93b503ce
heweiron <42639451+heweiron@users.noreply.github.com>
2020-02-11 14:19:03 -1000

    submission

---
+++ fiberoo.js
@@ -0,0 +1,28 @@
+
+function fib() {
+  const fibList = [];
+  let num = 0;
+  let num2 = 1;
+  for (let i = 0; i < 25; i++) {
+    fibList.push(num, num2);
+    num += num2;
+    num2 = num + num2;
+  }
+  return fibList;
+}
+console.log(fib());
+
+function numsToStrings() {
+  const newList = [];
+  _.map(fib(), function (entry) {
+    const ele = `${entry}`;
+    newList.push(ele);
+  });
+  return newList;
+}
+console.log(numsToStrings());
+
+function numEvenNums(list) {
+  return _.filter(list, function (num) { return num % 2 === 0; });
+}
+console.log(numEvenNums(fib()));
